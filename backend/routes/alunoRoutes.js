f3c9f0551be92d7fea7cfab8dc2774ca7e83b5e5
Pedro <167037774+PedroRosa14@users.noreply.github.com>
2025-05-23 09:39:11 -0300

    Add files via upload

--- backend/routes/alunoRoutes.js
+++ backend/routes/alunoRoutes.js
@@ -2,17 +2,24 @@ import express from "express";
 import {
   buscarAlunos,
   buscarAlunoId,
-  adicionarAluno,
+  adicionarAluno,  
   atualizarAluno,
   deletarAluno,
 } from "../controller/alunoController.js";
 
 const router = express.Router();
 
-router.get("/alunos", buscarAlunos);
-router.get("/alunos/:id", buscarAlunoId);
-router.post("/alunos", adicionarAluno);
-router.put("/alunos/:id", atualizarAluno);
-router.delete("/alunos/:id", deletarAluno);
+router.get("/", buscarAlunos);
+
+router.get("/:id", buscarAlunoId);
+
+
+router.post("/", adicionarAluno);
+
+
+router.put("/:id", atualizarAluno);
+
+
+router.delete("/:id", deletarAluno);
 
 export default router;
