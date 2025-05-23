c931babc076cc4410ba220d57fb06e6d7a9861f7
Pedro <167037774+PedroRosa14@users.noreply.github.com>
2025-05-23 09:08:35 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ import dotenv from "dotenv";
 
 import alunoRoutes from "./routes/alunoRoutes.js";
 import { sql } from "./config/db.js";
-import { setupSwagger } from "./swagger.js"; // Importe setupSwagger no topo
 
 dotenv.config();
 
@@ -20,8 +19,7 @@ app.use(morgan("dev"));
 // Rotas
 app.use("/", alunoRoutes);
 
-// Configuração do Swagger (DEVE VIR APÓS A DEFINIÇÃO DAS ROTAS)
-setupSwagger(app);
+
 
 // Criação da tabela no banco se não existir
 async function startdb() {
